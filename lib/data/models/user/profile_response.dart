8c34080d4c9db03c6dcce48dba24e1b886c8d824
hour <chhonhourseng@gmail.com>
2024-08-08 15:52:39 +0700

    - model

--- lib/data/models/user/profile_response.dart
+++ lib/data/models/user/profile_response.dart
@@ -1,16 +1,24 @@
 /// profile_response
 import 'package:agora/export.dart';
 import 'package:json_annotation/json_annotation.dart';
+part 'profile_response.g.dart';
+
+@JsonSerializable()
 class ProfileResponse {
   final String email;
   final String id;
+  @JsonKey(name: 'is_deleted')
   final bool isDeleted;
   final bool isVerify;
+  @JsonKey(name: 'phone_number')
   final String phoneNumber;
   final String profile;
-  final String registerCount;
-  final String totalBalance;
-  final String totalSpend;
+  @JsonKey(name: 'register_count')
+  final int registerCount;
+  @JsonKey(name: 'total_balance')
+  final int totalBalance;
+  @JsonKey(name: 'total_spend')
+  final int totalSpend;
   final String type;
   final String username;
 
@@ -30,34 +38,8 @@ class ProfileResponse {
 
   //coverFileName: json['cover_file_name'].toString().toAppString()!,
   // fromJson
-  factory ProfileResponse.fromJson(Map<String, dynamic> json) {
-    return ProfileResponse(
-      email: json["email"].toString().toAppString()!,
-      id: json["id"].toString().toAppString()!,
-      isDeleted: json["is_deleted"] == true,
-      isVerify: json["is_verify"] == true,
-      phoneNumber: json["phone_number"].toString().toAppString()!,
-      profile: json["profile"].toString().toAppString()!,
-      registerCount: json["register_count"].toString().toAppString()!,
-      totalBalance: json["total_balance"].toString().toAppString()!,
-      totalSpend: json["total_spend"].toString().toAppString()!,
-      type: json["type"].toString().toAppString()!,
-      username: json["username"].toString().toAppString()!,
-    );
-  }
+  factory ProfileResponse.fromJson(Map<String, dynamic> json) => _$ProfileResponseFromJson(json);
 
-// toJson
-  Map<String, dynamic> toJson() => {
-        "email": email,
-        "id": id,
-        "is_deleted": isDeleted,
-        "is_verify": isVerify,
-        "phone_number": phoneNumber,
-        "profile": profile,
-        "register_count": registerCount,
-        "total_balance": totalBalance,
-        "total_spend": totalSpend,
-        "type": type,
-        "username": username,
-      };
+  // toJson
+  Map<String, dynamic> toJson() => _$ProfileResponseToJson(this);
 }
---
+++ lib/data/models/user/profile_response.g.dart
@@ -0,0 +1,37 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'profile_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ProfileResponse _$ProfileResponseFromJson(Map<String, dynamic> json) =>
+    ProfileResponse(
+      email: json['email'] as String,
+      id: json['id'] as String,
+      isDeleted: json['is_deleted'] as bool,
+      isVerify: json['isVerify'] as bool,
+      phoneNumber: json['phone_number'] as String,
+      profile: json['profile'] as String,
+      registerCount: (json['register_count'] as num).toInt(),
+      totalBalance: (json['total_balance'] as num).toInt(),
+      totalSpend: (json['total_spend'] as num).toInt(),
+      type: json['type'] as String,
+      username: json['username'] as String,
+    );
+
+Map<String, dynamic> _$ProfileResponseToJson(ProfileResponse instance) =>
+    <String, dynamic>{
+      'email': instance.email,
+      'id': instance.id,
+      'is_deleted': instance.isDeleted,
+      'isVerify': instance.isVerify,
+      'phone_number': instance.phoneNumber,
+      'profile': instance.profile,
+      'register_count': instance.registerCount,
+      'total_balance': instance.totalBalance,
+      'total_spend': instance.totalSpend,
+      'type': instance.type,
+      'username': instance.username,
+    };
